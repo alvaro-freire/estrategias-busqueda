282cd79c108fb5bbcec8e4e0dd8f650e9515d926
alvaro-freire <alvaro.freirea@udc.es>
2022-03-16 12:29:38 +0100

    add Main for CuadradoMagico

---
+++ src/es/udc/sistemasinteligentes/MainCuadradoMagico.java
@@ -0,0 +1,16 @@
+package es.udc.sistemasinteligentes;
+
+public class MainCuadradoMagico {
+    public static void main(String[] args) throws Exception {
+        int n = 3;
+        int[][] cuadrado = {{4, 9, 0}, {0, 0, 0}, {0, 0, 0}}
+
+        ProblemaCuadradoMagico.EstadoCuadrado estadoInicial = new ProblemaCuadradoMagico.EstadoCuadrado(cuadrado, n);
+        ProblemaBusqueda cuadradoMagico = new ProblemaCuadradoMagico(estadoInicial);
+
+        EstrategiaBusquedaAnchura buscador = new EstrategiaBusquedaAnchura();
+        // EstrategiaBusquedaProfundidad buscador = new EstrategiaBusquedaProfundidad();
+
+        System.out.println(buscador.soluciona(cuadradoMagico));
+    }
+}
