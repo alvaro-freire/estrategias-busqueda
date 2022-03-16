dfafcbfce7a1bc15dda0e721c5f1c573a0eef628
alvaro-freire <alvaro.freirea@udc.es>
2022-03-16 12:43:57 +0100

    fix ;

--- src/es/udc/sistemasinteligentes/MainCuadradoMagico.java
+++ src/es/udc/sistemasinteligentes/MainCuadradoMagico.java
@@ -3,7 +3,7 @@ package es.udc.sistemasinteligentes;
 public class MainCuadradoMagico {
     public static void main(String[] args) throws Exception {
         int n = 3;
-        int[][] cuadrado = {{4, 9, 0}, {0, 0, 0}, {0, 0, 0}}
+        int[][] cuadrado = {{4, 9, 0}, {0, 0, 0}, {0, 0, 0}};
 
         ProblemaCuadradoMagico.EstadoCuadrado estadoInicial = new ProblemaCuadradoMagico.EstadoCuadrado(cuadrado, n);
         ProblemaBusqueda cuadradoMagico = new ProblemaCuadradoMagico(estadoInicial);
