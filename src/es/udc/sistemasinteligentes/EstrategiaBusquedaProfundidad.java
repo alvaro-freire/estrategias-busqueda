3c5b3b7d9410aa79aa7b7e2aab0a75ca02ccd559
mateo <mateodiaz780@gmail.com>
2022-03-17 19:21:13 +0100

    EstrategiaBusquedaProfundidad implemented

--- src/es/udc/sistemasinteligentes/EstrategiaBusquedaProfundidad.java
+++ src/es/udc/sistemasinteligentes/EstrategiaBusquedaProfundidad.java
@@ -1,15 +1,78 @@
 package es.udc.sistemasinteligentes;
 
-import java.util.ArrayList;
+import java.util.*;
 
 import es.udc.sistemasinteligentes.ejemplo.Nodo;
 
 public class EstrategiaBusquedaProfundidad implements EstrategiaBusqueda {
 
+    public Nodo[] reconstruyeSol(Nodo n) {
+        Nodo actual;
+        ArrayList<Nodo> revSol = new ArrayList<>();
+
+        actual = n;
+        while (actual != null) {
+            revSol.add(actual);
+            actual = actual.getPadre();
+        }
+
+        Collections.reverse(revSol);
+
+        return revSol.toArray(new Nodo[0]);
+    }
+
+    public boolean nodoEnLista(List<Nodo> list, Nodo nodo) {
+        for (Nodo nodoLista : list) {
+            if (nodoLista.getEstado() == nodo.getEstado()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean nodoEnPila(Stack<Nodo> list, Nodo nodo) {
+        for (Nodo nodoLista : list) {
+            if (nodoLista.getEstado() == nodo.getEstado()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     @Override
-    public Nodo[] soluciona(ProblemaBusqueda p) throws Exception {
-        // TODO Auto-generated method stub
-        return null;
+    public Nodo[] soluciona(ProblemaBusqueda p) {
+        List<Nodo> explorados = new ArrayList<>();
+        Nodo nodoActual = new Nodo(p.getEstadoInicial(), null, null);
+        Nodo nodoSiguiente;
+        Stack<Nodo> frontera = new Stack<>();
+        frontera.add(nodoActual);
+
+        while (true) {
+            if (frontera.isEmpty()) {
+                throw new IllegalArgumentException();
+            }
+
+            nodoActual = frontera.pop();
+
+            if (p.esMeta(nodoActual.getEstado())) {
+                break;
+            }
+
+            explorados.add(nodoActual);
+
+            for (Accion acc : p.acciones(nodoActual.getEstado())) {
+                nodoSiguiente =
+                        new Nodo(p.result(nodoActual.getEstado(), acc), nodoActual, acc);
+
+                if (!nodoEnPila(frontera, nodoSiguiente) && !nodoEnLista(explorados, nodoSiguiente)) {
+                    frontera.add(nodoSiguiente);
+                }
+            }
+        }
+
+        return reconstruyeSol(nodoActual);
     }
     
 }
