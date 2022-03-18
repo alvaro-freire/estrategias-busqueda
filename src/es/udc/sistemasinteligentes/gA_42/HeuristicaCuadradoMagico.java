68324245d9b126524dd6d2a8625f9d1a9449e92e
alvaro-freire <alvaro.freirea@udc.es>
2022-03-18 10:27:50 +0100

    add Heuristica

---
+++ src/es/udc/sistemasinteligentes/gA_42/HeuristicaCuadradoMagico.java
@@ -0,0 +1,8 @@
+package es.udc.sistemasinteligentes.gA_42;
+
+public class HeuristicaCuadradoMagico extends Heuristica{
+    @Override
+    public float evalua(Estado e) {
+        return 0;
+    }
+}
