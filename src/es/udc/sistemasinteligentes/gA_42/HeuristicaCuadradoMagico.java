2f5bc3cb48f21be0d15baa3c38c66d5d7d9f432a
alvaro-freire <alvaro.freirea@udc.es>
2022-03-18 22:31:11 +0100

    finish heuristica

--- src/es/udc/sistemasinteligentes/gA_42/HeuristicaCuadradoMagico.java
+++ src/es/udc/sistemasinteligentes/gA_42/HeuristicaCuadradoMagico.java
@@ -23,33 +23,42 @@ public class HeuristicaCuadradoMagico extends Heuristica {
         int n = ec.getN();
         int numMagico = n * (n * n + 1) / 2;
         float h = 1;
+        int sumaDiag1 = 0;
+        int sumaDiag2 = 0;
 
-        for (int[] fila : cuadrado) {
+        for (int i = 0; i < n; i++) {
             int casillasVacias = 0;
-            for (int casilla : fila) {
-                if (casilla == 0) {
+            for (int j = 0; j < n; j++) {
+                if (cuadrado[i][j] == 0) {
+                    casillasVacias++;
+                }
+
+                if (cuadrado[j][i] == 0) {
                     casillasVacias++;
                 }
             }
-            int suma = 0;
-            for (int num : fila) {
-                suma += num;
+            int sumaFila = 0;
+            int sumaCol = 0;
+            sumaDiag1 += cuadrado[i][i];
+            sumaDiag2 += cuadrado[i][n - i - 1];
+            for (int j = 0; j < n; j++) {
+                sumaFila += cuadrado[i][j];
+                sumaCol += cuadrado[j][i];
             }
 
-            if (suma > numMagico) {
+            if (sumaFila > numMagico || sumaCol > numMagico) {
                 return 0;
             }
-            if (casillasVacias > 0 && suma >= numMagico) {
+            if (casillasVacias > 0 && (sumaFila >= numMagico) || sumaCol >= numMagico) {
                 return 0;
             }
-            if (casillasVacias == 1 && contains(cuadrado, n, numMagico - suma)) {
+            if (casillasVacias == 1 && (contains(cuadrado, n, numMagico - sumaFila)
+                    || (contains(cuadrado, n, numMagico - sumaCol)))) {
                 return 0;
             }
-            if (casillasVacias == 0 && suma != numMagico) {
+            if (casillasVacias == 0 && (sumaFila != numMagico)) {
                 return 0;
             }
-
-            h *= (double) (n - casillasVacias) / n;
         }
 
         return h;
